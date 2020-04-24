835336a7fd8ef3abcd6976098c0afd6eddc139a6
NgugiMuthoni <ngugimuthoni43@gmail.com>
2020-04-24 23:04:26 +0300

    woorking on errors in test file

--- tests/text_sources.py
+++ tests/text_sources.py
@@ -6,17 +6,14 @@ class SourcesTest(unittest.TestCase):
     '''
     Test case to test the behavior of the Sources class
     '''
-
     def setUp(self):
         '''
         Setup function that will run before every test
         '''
         self.new_source = Sources('mynews', 'My News', 'We have the latest updates', 'https://google.com', 'general',
                                   'ke')
-
     def test_instance(self):
         self.assertTrue(isinstance(self.new_source, Sources))
-
     def test_to_check_instance_variables(self):
         '''
         Test function to check instance variables
