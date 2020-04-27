6a74e5f574c73f75e4a886d59b95c2dc28c6a7d0
NgugiMuthoni <ngugimuthoni43@gmail.com>
2020-04-28 00:09:13 +0300

    fixing display error

--- app/main/views.py
+++ app/main/views.py
@@ -13,6 +13,7 @@ def index():
     technology_news = get_sources('technology')
     science_news = get_sources('science')
     health_news = get_sources('health')
+
     title = 'Home-Best News Update Site'
 
     return render_template('index.html', title=title, General=general_news, Business=business_news,
